ef9db3aab24b163ec1c654d3a44db7f68ab4aedd
divineLush <alvipath@yahoo.com>
2024-08-01 17:58:51 +0700

    setting up header

--- frenschat-frontend/src/views/HomeView.vue
+++ frenschat-frontend/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    home
   </main>
 </template>
