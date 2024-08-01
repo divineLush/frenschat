af19f3a69d52c2d5fe93345e1bc0dd85ede218a8
divineLush <alvipath@yahoo.com>
2024-08-01 15:17:20 +0000

    signup form

--- frenschat-frontend/src/views/RegisterView.vue
+++ frenschat-frontend/src/views/RegisterView.vue
@@ -1,9 +1,21 @@
 <template>
-  <form>
-    <label>Email</label>
-    <input></input>
+  <form class="register-form">
+    <label for="register-email">Email</label>
+    <input id="register-email" type="email" required></input>
 
-    <label>Password</label>
-    <input type="password"></input>
+    <label for="register-password">Password</label>
+    <input id="register-password" type="password" required></input>
+
+    <button type="submit">SIGN UP</button>
   </form>
 </template>
+
+<style>
+.register-form {
+  margin: 0 auto;
+  max-width: 400px;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+}
+</style>
