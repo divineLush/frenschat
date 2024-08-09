e5f091b6ce5ea345171b6aed9898f23a2891e42b
divineLush <alvipath@yahoo.com>
2024-08-09 09:30:14 +0000

    broadcasting messages to all connected clients

--- frenschat-backend/router/room.js
+++ frenschat-backend/router/room.js
@@ -1,10 +1,12 @@
 export default (fastify, _, done) => {
   fastify.get('/', { websocket: true }, async (socket, request) => {
-    // reply.header("Access-Control-Allow-Origin", "*")
-    // reply.header("Access-Control-Allow-Methods", "POST")
 
     socket.on('message', message => {
-      socket.send(`${message.toString()} from server`)
+      fastify.websocketServer.clients.forEach((client) => {
+        if (client.readyState === 1) {
+          client.send(`${message.toString()}`)
+        }
+      })
     })
   })
 
